426c633d82284beed62df4d135a24ee81ab2bb41
inn@ascot.ac.th <inn@ascot.ac.th>
2021-03-15 12:17:06 +0700

    =ds

---
+++ Assessment2-FlashCards/Card.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2_FlashCards
+{
+    class Card
+    {
+        private string question;
+        private string answer;
+        private bool flipped;
+
+        public Card(string question,string answer)
+        {
+            this.question = question;
+            this.answer = answer;
+        }
+
+        public void flipCard()
+        {
+            if (flipped)
+            {
+                flipped = false;
+            }
+            else
+            {
+                flipped = true;
+            }
+        }
+
+        public string getCardText()
+        {
+            if(flipped)
+            {
+                return answer;
+            }
+            else 
+            {
+                return question;
+            }
+        }
+
+        public bool isFlipped()
+        {
+            return flipped;
+        }
+    }
+}
